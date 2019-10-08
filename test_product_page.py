5d308ca05840f1f158830e412cbdff88e4bc0bf6
FrendsTeamApp <FrendsTeamApp@gmail.com>
2019-10-08 16:09:50 +0300

    test add produt to basket

--- test_product_page.py
+++ test_product_page.py
@@ -4,7 +4,7 @@ from .pages.product_page import ProductPage
 link = "http://selenium1py.pythonanywhere.com/catalogue/the-shellcoders-handbook_209/?promo=newYear"
 
 
-def test_product_page_add_to_busket(browser):
+def test_guest_can_add_product_to_basket(browser):
     page = ProductPage(browser, link)
     page.open()
     page.should_be_add_button()
