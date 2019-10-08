e2e455efd6210bbfbe07e5009ede729f055f3553
FrendsTeamApp <FrendsTeamApp@gmail.com>
2019-10-08 12:03:59 +0300

    create conftest

--- conftest.py
+++ conftest.py
@@ -28,12 +28,12 @@ def browser(request):
     user_language = request.config.getoption("language")
     browser = None
     if browser_name == "chrome":
-        print(f"\nuser language: {user_language}\nstart chrome browser for test..")
+        print(f"\nuser language: {user_language}\nstart {browser_name} browser for test..")
         options = Options()
         options.add_experimental_option('prefs', {'intl.accept_languages': user_language})
         browser = webdriver.Chrome(options=options)
     elif browser_name == "firefox":
-        print("\nstart firefox browser for test..")
+        print(f"\nuser language: {user_language}\nstart {browser_name} browser for test..")
         fp = webdriver.FirefoxProfile()
         fp.set_preference("intl.accept_languages", user_language)
         browser = webdriver.Firefox(firefox_profile=fp)
