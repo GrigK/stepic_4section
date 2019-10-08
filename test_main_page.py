8c8e749c6ed240435cb5c46f7f73fd0bb143231d
FrendsTeamApp <FrendsTeamApp@gmail.com>
2019-10-08 13:58:20 +0300

    add assert message

--- test_main_page.py
+++ test_main_page.py
@@ -8,14 +8,12 @@ def test_guest_goto_login_page(browser):
     page.go_to_login_page()
 
 
-# def go_to_login_page(browser):
-#     link = browser.find_element_by_css_selector("#login_link")
-#     link.click()
-#
-#
-# def test_guest_can_go_to_login_page(browser):
-#     browser.get(link)
-#     go_to_login_page(browser)
+def test_guest_should_see_login_link(browser):
+    link = "http://selenium1py.pythonanywhere.com/"
+    page = MainPage(browser, link)
+    page.open()
+    page.should_be_login_link()
+
 
 """
 Рассмотрим страницу товара в интернет магазине http://selenium1py.pythonanywhere.com/catalogue/the-shellcoders-handbook_209/.
