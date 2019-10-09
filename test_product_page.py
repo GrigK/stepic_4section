bdaf1f50949289b856a436b35eb87bc86b0ee927
FrendsTeamApp <FrendsTeamApp@gmail.com>
2019-10-09 20:03:51 +0300

    fix suffix to format string

--- test_product_page.py
+++ test_product_page.py
@@ -13,7 +13,7 @@ link = "http://selenium1py.pythonanywhere.com/catalogue/the-shellcoders-handbook
                                     pytest.param(7, marks=pytest.mark.xfail),
                                     8, 9])
 def test_guest_can_add_product_to_basket(browser, suffix):
-    uri = "http://selenium1py.pythonanywhere.com/catalogue/coders-at-work_207/?promo=offer" + str(suffix)
+    uri = f"http://selenium1py.pythonanywhere.com/catalogue/coders-at-work_207/?promo=offer{suffix}"
     page = ProductPage(browser, uri)
     page.open()
     page.add_product_to_basket_and_calculate()
